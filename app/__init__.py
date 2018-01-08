b9b35afbab8447e18f123611799b467cc8ace0f8
David Ficociello <davidfic@gmail.com>
2018-01-07 23:20:10 -0500

    updating for datastore

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,9 @@
 from flask import Flask
 from flask import session
-from flask_debugtoolbar import DebugToolbarExtension
-# from pymongo import MongoClient
 
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
-
-
-# connection = MongoClient("ds047448.mlab.com", 47448)
-# db = connection["theabbytapes"]
-# # MongoLab has user authentication
-# db.authenticate("dfic2", "password")
 
 
 from app import views
